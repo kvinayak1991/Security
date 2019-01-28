6297faba836d8bf16b3b0d0ee36dd64a1511547e
kvinayak1991 <32790386+kvinayak1991@users.noreply.github.com>
2019-01-28 15:55:46 +0530

    Update EncryptionDescyprionUtility.java

--- EncryptionDescyprionUtility.java
+++ EncryptionDescyprionUtility.java
@@ -53,14 +53,4 @@ public class EncryptionDescyprionUtility {
 }
 
 
-//JS CODE with npm install crypto-js --save in angular 6
 
-//var message = "password";
-//var key = "aesEncryptionKey"; //length=22
-//var iv  = "encryptionIntVec"; //length=22
-
-//key = CryptoJS.enc.Utf8.parse(key);iv = CryptoJS.enc.Utf8.parse(iv);
-//var cipherData = CryptoJS.AES.encrypt(message, key, { iv: iv });
-//console.log(cipherData.toString());
-//var data = CryptoJS.AES.decrypt(cipherData, key, { iv: iv });
-//console.log("Original String:::"+data.toString(CryptoJS.enc.Utf8));
